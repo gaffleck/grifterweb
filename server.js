b7791035caa98b60b4cab8c2dae233d5c2cc3a25
Geoff Affleck <GAffleck@rbauction.com>
2018-12-04 05:52:41 -0800

    catch all route

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
